7c2cdabfc6c2fb12f741581128fc9f809b172b8a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-28 15:17:27 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Lab/05_PrintEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_PrintEvenNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbersInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> numbers = new Queue<int>(numbersInput);
+
+            Console.WriteLine(string.Join(", ", numbers.Where(x => x % 2 == 0)));
+        }
+    }
+}
