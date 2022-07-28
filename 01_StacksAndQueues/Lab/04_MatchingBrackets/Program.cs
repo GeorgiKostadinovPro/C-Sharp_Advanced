9a0640e92251827d7ac96baa24b7c991a69e135f
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-28 15:17:12 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Lab/04_MatchingBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_MatchingBrackets
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputExpression = Console.ReadLine();
+
+            Stack<int> expressionIndexes = new Stack<int>();
+
+            for (int i = 0; i < inputExpression.Length; i++)
+            {
+                char symbol = inputExpression[i];
+
+                if (symbol == '(')
+                {
+                    expressionIndexes.Push(i);
+                }
+                else if (symbol == ')')
+                {
+                    int index = expressionIndexes.Pop();
+
+                    string exp = inputExpression.Substring(index, i - index + 1);
+                    Console.WriteLine(exp);
+                }
+            }
+        }
+    }
+}
