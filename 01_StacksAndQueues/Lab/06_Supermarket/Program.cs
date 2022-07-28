12c27302d1a407f166fbda87365adc0c5134abe1
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-28 15:17:40 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Lab/06_Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Supermarket
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = string.Empty;
+
+            Queue<string> customers = new Queue<string>();
+
+            while ((line = Console.ReadLine()) != "End")
+            {
+                string input = line;
+
+                if (input == "Paid")
+                {
+                    while (customers.Count > 0)
+                    {
+                        string customer = customers.Dequeue();
+                        Console.WriteLine(customer);
+                    }
+                }
+                else 
+                {
+                    customers.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
