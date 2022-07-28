e92afe6f7f577e020f0a3e9260f78ec9c73ba7bf
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-28 15:15:39 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Lab/01_ReverseAString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_ReverseAString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            Stack<char> reversedStr = new Stack<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                reversedStr.Push(str[i]);
+            }
+
+            while (reversedStr.Count > 0)
+            {
+                char currElement = reversedStr.Pop();
+                Console.Write(currElement);
+            }
+        }
+    }
+}
---
+++ 01_StacksAndQueues/Lab/02_StackSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_StackSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numbers = new Stack<int>(inputNumbers);
+
+            while (true)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .ToLower()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string cmd = cmdArgs[0];
+
+                if (cmd == "end")
+                {
+                    break;
+                }
+
+                if (cmd == "add")
+                {
+                    int firstNumber = int.Parse(cmdArgs[1]);
+                    int secondNumber = int.Parse(cmdArgs[2]);
+
+                    numbers.Push(firstNumber);
+                    numbers.Push(secondNumber);
+                }
+                else if (cmd == "remove")
+                {
+                    int countOfRemovedNums = int.Parse(cmdArgs[1]);
+
+                    if (numbers.Count >= countOfRemovedNums)
+                    {
+                        for (int i = 0; i < countOfRemovedNums; i++)
+                        {
+                            numbers.Pop();
+                        }
+                    }
+                }
+            }
+
+            int sum = numbers.Sum();
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
