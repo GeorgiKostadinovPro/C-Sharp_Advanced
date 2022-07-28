197c60302d418585c6caa70e0e498a83ca46b5e3
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-28 15:17:56 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Lab/07_HotPotato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07_HotPotato
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] kidsInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> kids = new Queue<string>(kidsInput);
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    string kid = kids.Dequeue();
+                    kids.Enqueue(kid);
+                }
+
+                string loser = kids.Dequeue();
+                Console.WriteLine($"Removed {loser}");
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
