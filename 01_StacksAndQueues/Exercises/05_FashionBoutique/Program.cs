f0306eff3dd55c587910c230dea55f506995ae9a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-29 16:06:43 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Exercises/05_FashionBoutique/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_FashionBoutique
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           int[] clothes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int capacityOfRack = int.Parse(Console.ReadLine());
+
+            Stack<int> box = new Stack<int>(clothes);
+
+            int racksCount = 1;
+
+            int sum = 0;
+
+            while (box.Count > 0)
+            {
+                int cloth = box.Peek();
+
+                sum += cloth;
+
+                if (sum <= capacityOfRack)
+                {
+                    box.Pop();
+                }
+                else
+                {
+                    racksCount++;
+                    sum = 0;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
