ed5524ac510b75c08651101719d1d61261ae8f33
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-29 16:06:27 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Exercises/04_FastFood/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_FastFood
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine()); 
+
+            Queue<int> orderQuantity = new Queue<int>(Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray());
+
+            int tempSum = 0;
+
+            Console.WriteLine(orderQuantity.Max());
+
+            while (orderQuantity.Count > 0)
+            {
+                int order = orderQuantity.Peek();
+                tempSum += order;
+                if (tempSum <= foodQuantity)
+                {
+                    orderQuantity.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orderQuantity)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
