5d1a301ce6f21aa95d96088c1b13a125ebab680d
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-29 16:07:19 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Exercises/07_TruckTour/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07_TruckTour
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var petrol = new Queue<int>();
+            var distance = new Queue<int>();
+            int[] input;
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                petrol.Enqueue(input[0]);
+                distance.Enqueue(input[1]);
+            }
+
+            int currentFuel;
+
+            Queue<int> petrolCopy = new Queue<int>();
+            Queue<int> distanceCopy = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                currentFuel = petrol.Peek();
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (distance.Peek() <= currentFuel)
+                    {
+                        currentFuel -= distance.Peek();
+
+                        if (j == n - 1)
+                        {
+                            Console.WriteLine(i);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        for (int k = j; k < n; k++)
+                        {
+                            petrol.Enqueue(petrol.Dequeue());
+                            distance.Enqueue(distance.Dequeue());
+                        }
+
+                        break;
+                    }
+
+                    petrol.Enqueue(petrol.Dequeue());
+                    distance.Enqueue(distance.Dequeue());
+                    currentFuel += petrol.Peek();
+                }
+
+                petrol.Enqueue(petrol.Dequeue());
+                distance.Enqueue(distance.Dequeue());
+            }
+        }
+    }
+}
