51783b1b8f117c02bdb5e5f5941dee17123c1b56
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-29 16:06:13 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Exercises/03_MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_MaximumAndMinimumElement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] query = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string cmd = query[0];
+
+                switch (cmd)
+                {
+                    case "1":
+                        stack.Push(int.Parse(query[1]));
+                        break;
+                    case "2":
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    case "3":
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case "4":
+                        if (stack.Count > 0)
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
