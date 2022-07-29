17047892176fd8f14d3ff8fbab0032a0a1b1d416
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-07-29 16:05:50 +0300

    Add files via upload

---
+++ 01_StacksAndQueues/Exercises/01_BasicStackOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_BasicStackOperations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int numberOfElementsToPush = data[0];
+            int numberOfElementsToPop = data[1];
+            int toSearch = data[2];
+
+            Stack<int> stack = new Stack<int>(
+                Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+
+            while (numberOfElementsToPop > 0 && stack.Count > 0)
+            {
+                stack.Pop();
+                numberOfElementsToPop--;
+            }
+
+            if (stack.Contains(toSearch))
+            {
+                Console.WriteLine("true");
+            }
+            else 
+            {
+                if (stack.Any())
+                {
+                    Console.WriteLine(stack.Min());
+                }
+                else 
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
---
+++ 01_StacksAndQueues/Exercises/02_BasicQueueOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_BasicQueueOperations
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int numberOfElementsToPush = data[0];
+            int numberOfElementsToPop = data[1];
+            int toSearch = data[2];
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+
+            while (numberOfElementsToPop > 0 && queue.Count > 0)
+            {
+                queue.Dequeue();
+                numberOfElementsToPop--;
+            }
+
+            if (queue.Contains(toSearch))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Any())
+                {
+                    Console.WriteLine(queue.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+    
+}
