6e694d015c91c35b01de365d992ae280ff89e8f6
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-01 14:47:23 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Lab/03_Largest3Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Largest3Numbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            numbers = numbers.OrderByDescending(x => x).ToArray();
+
+            numbers = numbers.Length > 2 ? numbers.Take(3).ToArray() : numbers;
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
