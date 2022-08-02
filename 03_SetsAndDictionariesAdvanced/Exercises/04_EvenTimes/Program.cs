b32101603f1da23838491f23c6ce95544c815802
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:42:45 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/04_EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_EvenTimes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> evenTimes = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!evenTimes.ContainsKey(num))
+                {
+                    evenTimes.Add(num, 0);
+                }
+
+                evenTimes[num]++;
+            }
+
+            var result = evenTimes.Where(kvp => kvp.Value % 2 == 0).FirstOrDefault();
+
+            Console.WriteLine(result.Key);
+
+        }
+    }
+}
