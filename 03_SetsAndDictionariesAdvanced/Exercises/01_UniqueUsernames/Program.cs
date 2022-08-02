6ef05c4587ec3b1196e71f0d631c348b3b400744
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:42:12 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/01_UniqueUsernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_UniqueUsernames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+
+                usernames.Add(username);
+            }
+
+            foreach (var username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
---
+++ 03_SetsAndDictionariesAdvanced/Exercises/02_SetsOfElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_SetsOfElements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            for (int i = 0; i < n + m; i++)
+            {
+                int el = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    first.Add(el);
+                }
+                else
+                {
+                    second.Add(el);
+                }
+            }
+
+            first.IntersectWith(second);
+
+            Console.WriteLine(string.Join(" ", first));
+        }
+    }
+}
