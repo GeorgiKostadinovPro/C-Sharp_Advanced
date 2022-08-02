6ebb0f29c9d02eb56da93e07d22c7194f2908fba
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:45:15 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/10_ForceBook/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10_ForceBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string line = string.Empty;
+            
+            Dictionary<string, string> forceUsers = new Dictionary<string, string>();
+            Dictionary<string, List<string>> forceSideMemebers = new Dictionary<string, List<string>>();
+            
+            while ((line = Console.ReadLine()) != "Lumpawaroo")
+            {
+
+                if (line.Contains("|"))
+                {
+                    string[] cmdArgs = line
+                    .Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string forceSide = cmdArgs[0];
+                    string forceUser = cmdArgs[1];
+
+                    if (!forceUsers.ContainsKey(forceUser))
+                    {
+                        forceUsers.Add(forceUser, forceSide);
+                    }
+                }
+                else if (line.Contains("->"))
+                {
+                    string[] cmdArgs = line
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string forceUser = cmdArgs[0];
+                    string forceSide = cmdArgs[1];
+
+                    if (!forceUsers.ContainsKey(forceUser))
+                    {
+                        forceUsers.Add(forceUser, null);
+                    }
+
+                    forceUsers[forceUser] = forceSide;
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+            }
+
+            foreach (var forceUser in forceUsers)
+            {
+                if (!forceSideMemebers.ContainsKey(forceUser.Value))
+                {
+                    forceSideMemebers.Add(forceUser.Value, new List<string>());
+                }
+
+                forceSideMemebers[forceUser.Value].Add(forceUser.Key);
+            }
+
+            foreach (var forceSide in forceSideMemebers.OrderByDescending(s => s.Value.Count).ThenBy(s => s.Key))
+            {
+                Console.WriteLine($"Side: {forceSide.Key}, Members: {forceSide.Value.Count}");
+
+                foreach (var forceUser in forceSide.Value.OrderBy(u => u))
+                {
+                    Console.WriteLine($"! {forceUser}");
+                }
+            }
+        }
+    }
+}
