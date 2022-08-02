60d8296751c2aa32b46cea6dfe9152102ff4d895
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:43:08 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/05_CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_CountSymbols
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> counter = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!counter.ContainsKey(text[i]))
+                {
+                    counter.Add(text[i], 0);
+                }
+
+                counter[text[i]]++;
+            }
+
+            foreach (var ch in counter)
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
