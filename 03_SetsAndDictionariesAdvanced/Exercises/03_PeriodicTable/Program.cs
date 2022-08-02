e2601b360c7e32dbd38f6f7a01eae4637a9d1fad
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:42:32 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/03_PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_PeriodicTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> table = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    table.Add(elements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", table.OrderBy(e => e)));
+        }
+    }
+}
