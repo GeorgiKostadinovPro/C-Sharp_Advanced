57745531688d7cfd3bdb2cfd89025fdce9394200
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-02 14:43:20 +0300

    Add files via upload

---
+++ 03_SetsAndDictionariesAdvanced/Exercises/06_Wardrobe/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Wardrobe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine()
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string color = info[0];
+
+                string[] clothing = info[1]
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                foreach (var cloth in clothing)
+                {
+                    if (!wardrobe[color].ContainsKey(cloth))
+                    {
+                        wardrobe[color].Add(cloth, 0);
+                    }
+
+                    wardrobe[color][cloth]++;
+                }
+            }
+
+            string[] cmd = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var kvp in wardrobe)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+
+                foreach (var cloth in kvp.Value)
+                {
+                    string result = kvp.Key == cmd[0] && cloth.Key == cmd[1] ? $"* {cloth.Key} - {cloth.Value} (found!)" : $"* {cloth.Key} - {cloth.Value}";
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
