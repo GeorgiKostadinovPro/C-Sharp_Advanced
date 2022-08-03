438044180ac202554c20dc5d38b0b2e9890ee77b
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-03 17:01:59 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Lab/05_ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtractSpecialBytes
+{
+   
+    public class ExtractSpecialBytes
+    {
+        static void Main(string[] args)
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            byte[] imageBytes = File.ReadAllBytes(binaryFilePath);
+
+            string[] bytesFile = File.ReadAllLines(bytesFilePath);
+
+            List<byte> allBytes = new List<byte>();
+
+            for (int i = 0; i < imageBytes.Length; i++)
+            {
+                if (bytesFile.Contains(imageBytes[i].ToString()))
+                {
+                    allBytes.Add(imageBytes[i]);
+                }
+            }
+
+            File.WriteAllBytes(outputPath, allBytes.ToArray());
+        }
+    }
+}
