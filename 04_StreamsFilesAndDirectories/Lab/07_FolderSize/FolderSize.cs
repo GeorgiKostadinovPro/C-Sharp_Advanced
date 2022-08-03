e1336fb0141155445c73c33724d25a8b1154de08
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-03 17:03:51 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Lab/07_FolderSize/FolderSize.cs
@@ -0,0 +1,29 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+
+            double size = directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories).Sum(f => f.Length);
+
+            size /= 1024;
+
+            File.WriteAllText(outputFilePath, size.ToString());
+        }
+    }
+}
