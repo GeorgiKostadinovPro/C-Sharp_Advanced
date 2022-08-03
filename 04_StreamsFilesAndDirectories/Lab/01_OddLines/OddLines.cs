4b25110e39826b218f7785032b1a23d29c747a04
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-03 16:57:38 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Lab/01_OddLines/OddLines.cs
@@ -0,0 +1,48 @@
+﻿namespace OddLines
+{
+    using System.Collections.Generic;
+    using System.IO;
+	
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            List<string> oddLines = new List<string>();
+
+            int lines = 0;
+
+            string text = string.Empty;
+
+            while ((text = reader.ReadLine()) != null)
+            {
+                if (lines % 2 != 0)
+                {
+                    oddLines.Add(text);
+                }
+
+                lines++;
+            }
+
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            foreach (var line in oddLines)
+            {
+                writer.WriteLine(line);
+            }
+
+            writer.Close();
+        }
+    }
+}
---
+++ 04_StreamsFilesAndDirectories/Lab/02_LineNumbers/LineNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace LineNumbers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            List<string> lines = new List<string>();
+
+            string text = string.Empty;
+
+            while ((text = reader.ReadLine()) != null)
+            {
+                lines.Add(text);
+            }
+
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                writer.WriteLine($"{i + 1}. {lines[i]}");
+            }
+
+            writer.Close();
+        }
+    }
+}
