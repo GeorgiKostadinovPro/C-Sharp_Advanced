5e8e359781e7fe63a4dbd8cb96faa38a78437357
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-04 14:31:32 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Exercises/03_CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\copyMe.png";
+            string outputPath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream stream = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create)) 
+                { 
+                    byte[] buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        int currByte = stream.Read(buffer, 0, buffer.Length);
+
+                        if (currByte <= 0)
+                        {
+                            break;
+                        } 
+                        
+                        writer.Write(buffer, 0, buffer.Length); 
+                    }
+                }
+            }
+        }
+    }
+}
