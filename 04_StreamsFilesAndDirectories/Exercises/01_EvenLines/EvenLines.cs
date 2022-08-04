37c12d55d6b2e8489f6c38c1bf31808d37e1e1f6
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-04 14:30:45 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Exercises/01_EvenLines/EvenLines.cs
@@ -0,0 +1,66 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            string line = string.Empty;
+            int n = 0;
+
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (n % 2 == 0)
+                {
+                    string replaced = ReplaceSymbols(line);
+                    replaced = ReverseWords(replaced);
+                    sb.AppendLine(replaced);
+                }
+
+                n++;
+            }
+
+            return sb.ToString().TrimEnd();   
+        }
+        private static string ReverseWords(string line)
+        {
+            var result = line.Split(' ').Reverse();
+            return string.Join(" ", result);
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            char[] symbolstoReplace = { '.', ',', '-', '!', '?' };
+
+            string result = string.Empty;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (symbolstoReplace.Contains(line[i]))
+                {
+                    line = line.Replace(line[i], '@');
+                    result = line;
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ 04_StreamsFilesAndDirectories/Exercises/02_LineNumbers/LineNumbers.cs
@@ -0,0 +1,50 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] data = File.ReadAllLines(inputFilePath);
+
+            char[] symbols = { '.', ',', '!', '?', ':', '-', '\'', '"', '(', ')', '[', ']', ';' };
+
+            string[] result = new string[data.Length];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int currSignsCount = 0;
+                int currLettersCount = 0;
+
+                foreach (var symbol in symbols)
+                {
+                    currSignsCount += data[i].Count(e => e == symbol);
+                }
+
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (char.IsLetter(data[i][j]))
+                    {
+                        currLettersCount++;
+                    }
+                }
+
+                string line = $"Line {i+1} :{data[i]}({currLettersCount})({currSignsCount})";
+                result[i] = line;
+            }
+
+            File.WriteAllLines(outputFilePath, result);
+        }
+    }
+}
