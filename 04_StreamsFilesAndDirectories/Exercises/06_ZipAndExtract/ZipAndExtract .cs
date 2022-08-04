db80b9cd73e08cd455d81061961633cc2377a3f7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-04 14:33:30 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Exercises/06_ZipAndExtract/ZipAndExtract .cs
@@ -0,0 +1,37 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using ZipArchive file = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+
+            file.CreateEntryFromFile(inputFilePath, "copyMe.png");
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using ZipArchive file = ZipFile.OpenRead(zipArchiveFilePath);
+
+            var currZip = file.GetEntry(fileName);
+
+            currZip.ExtractToFile(outputFilePath);
+        }
+    }
+}
