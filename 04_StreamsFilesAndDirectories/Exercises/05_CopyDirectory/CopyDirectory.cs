5b402b42d3da41d3ca834d0d434e59fc9cc28818
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-04 14:33:03 +0300

    Add files via upload

---
+++ 04_StreamsFilesAndDirectories/Exercises/05_CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,21 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = Console.ReadLine();
+            string outputPath = Console.ReadLine();
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            Directory.Move(inputPath, outputPath);
+        }
+    }
+}
