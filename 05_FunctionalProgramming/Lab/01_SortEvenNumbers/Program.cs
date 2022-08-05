193a7b7fb94d4cc220829a4ae89e3a9bbbe94518
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-05 13:57:25 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Lab/01_SortEvenNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_SortEvenNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[]> func = () => numbers
+                             .Where(x => x % 2 == 0)
+                             .OrderBy(x => x)
+                             .ToArray();
+
+            Console.WriteLine(string.Join(", ", func()));
+        }
+    }
+}
---
+++ 05_FunctionalProgramming/Lab/02_SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_SumNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
