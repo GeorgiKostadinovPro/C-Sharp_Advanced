15e6fd176797fa160cade26d64e7b3d011b0ec3c
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-05 13:58:14 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Lab/04_AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_AddVAT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<string, double> parser = (el) => double.Parse(el);
+            Func<double, double> VAT = (el) => el + el * 0.2;
+
+            List<double> prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .Select(VAT)
+                .ToList();
+
+            prices.ForEach(price => Console.WriteLine($"{price:f2}"));
+        }
+    }
+}
