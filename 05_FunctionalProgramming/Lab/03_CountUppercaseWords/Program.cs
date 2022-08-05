bfa15f9fc6b3c591e4e3d8ab81cf7d78e8b70468
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-05 13:57:44 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Lab/03_CountUppercaseWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03_CountUppercaseWords
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Func<string, bool> uppercaseWords = (text) => {
+                string[] words = text
+                   .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in words)
+                {
+                    return char.IsUpper(word[0]);
+                }
+
+                return false;
+            };
+
+            foreach (var word in text.Split(' '))
+            {
+                if (uppercaseWords(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
