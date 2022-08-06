9227401fc9ff17f31a56ba204709da4e2014b067
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:39:20 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/11_TriFunction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _11_TriFunction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+              .ToArray();
+
+            Func<string, int, bool> baseLogic = (x, y) =>
+            {
+                int sum = 0;
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    sum += (int)x[i];
+                }
+
+                if (sum >= y)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            names = names.Where(x => baseLogic(x, n)).ToArray();
+
+            Console.WriteLine(names.FirstOrDefault());
+        }
+    }
+}
