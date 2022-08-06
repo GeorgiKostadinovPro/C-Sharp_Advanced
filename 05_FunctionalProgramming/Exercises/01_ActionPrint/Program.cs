b68db1f46e6fc967fc19133d4cdd0d3ae7d6561c
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:37:08 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/01_ActionPrint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_ActionPoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> print = (x) => Console.WriteLine(string.Join("\n", x));
+            print(names);
+        }
+    }
+}
---
+++ 05_FunctionalProgramming/Exercises/02_KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_KnightsOfhonor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<string> print = (x) => Console.WriteLine($"Sir {x}");
+
+            names.ForEach(print);
+        }
+    }
+}
