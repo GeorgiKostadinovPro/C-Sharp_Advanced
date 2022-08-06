4b852a90e3f36f0919da9e755f10edcb2d35c1be
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:37:50 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/03_CustomMinFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_CustomMinFunction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> min = (x) =>
+            {
+                int min = int.MaxValue;
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    if (x[i] < min)
+                    {
+                        min = x[i];
+                    }
+                }
+
+                return min;
+            };
+
+            Console.WriteLine(min(numbers));
+        }
+    }
+}
---
+++ 05_FunctionalProgramming/Exercises/04_FindEvensOrOdds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_FindEvenOrOdds
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string cmd = Console.ReadLine();
+
+            Predicate<int> isEven = x => x % 2 == 0;
+
+            Func<int[], string, List<int>> func = (x, y) =>
+            {
+                var result = new List<int>();
+
+                for (int i = x[0]; i <= x[x.Length - 1]; i++)
+                {
+                    result.Add(i);
+                }
+
+                if (cmd == "even")
+                {
+                    result = result.Where(x => isEven(x)).ToList();
+                }
+                else
+                {
+                    result = result.Where(x => !isEven(x)).ToList();
+                }
+
+                return result;
+            };
+
+            Console.WriteLine(string.Join(" ", func(bounds, cmd)));
+        }
+    }
+}
