a865a6304ed4ad91af31a5f0e982022ec8def035
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:38:03 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/05_AppliedArithmetics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_AppliedArithmetics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            string line = string.Empty;
+
+            Func<int, int> add = (x) => x + 1;
+            Func<int, int> multiply = (x) => x * 2;
+            Func<int, int> subtract = (x) => x - 1;
+
+            Action<int[]> print = (x) => Console.WriteLine(string.Join(" ", x));
+
+            while ((line = Console.ReadLine()) != "end")
+            {
+                if (line == "add")
+                {
+                    numbers = numbers.Select(add).ToArray();
+                }
+                else if (line == "multiply")
+                {
+                    numbers = numbers.Select(multiply).ToArray();
+                }
+                else if (line == "subtract")
+                {
+                    numbers = numbers.Select(subtract).ToArray();
+                }
+                else if (line == "print")
+                {
+                    print(numbers);
+                }
+            }
+        }
+    }
+}
