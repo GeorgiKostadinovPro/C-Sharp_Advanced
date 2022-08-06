de15cb48dbef42a8eb343fed09839a4d2400ca14
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:38:32 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/07_PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_PredicatesForNames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+
+            Predicate<string> check = (x) => x.Length <= length;
+
+            Action<string[]> print = (x) =>
+            {
+                foreach (var item in x)
+                {
+                    Console.WriteLine(item);
+                }
+            };
+
+            names = names.Where(x => check(x)).ToArray();
+
+            print(names);
+        }
+    }
+}
