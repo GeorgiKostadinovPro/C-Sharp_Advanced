96e2344c59b5c605581bc428b4f923314e2393d3
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-06 13:38:16 +0300

    Add files via upload

---
+++ 05_FunctionalProgramming/Exercises/06_ReverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_ReverseAndExclude
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Action<int[]> reverse = (x) => Array.Reverse(x);
+
+            reverse(numbers);
+
+            Predicate<int> filter = (x) => x % n == 0;
+
+            numbers = numbers.Where(x => !filter(x)).ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
