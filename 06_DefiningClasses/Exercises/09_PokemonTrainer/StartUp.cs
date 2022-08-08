103c3ea30c0ef6000a5209e9736328b77a640908
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-08 17:21:26 +0300

    Add files via upload

---
+++ 06_DefiningClasses/Exercises/09_PokemonTrainer/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using PokemonTrainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarSalesman
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "Tournament")
+            {
+                string[] cmdArgs = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = cmdArgs[0];
+                string pokemonName = cmdArgs[1];
+                string pokemonElement = cmdArgs[2];
+                int pokemonHealth = int.Parse(cmdArgs[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                Trainer trainer = trainers
+                    .FirstOrDefault(t => t.Name == trainerName);
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(trainerName);
+                    trainers.Add(trainer);
+                }
+
+                trainer.Pokemons.Add(pokemon);
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Any(p => p.Element == command))
+                    {
+                        trainer.NumberOfBadges++;
+                    }
+                    else
+                    {
+                        foreach (var pokemon in trainer.Pokemons)
+                        {
+                            pokemon.Health -= 10;
+                        }
+                    }
+
+                    trainer.Pokemons.RemoveAll(p => p.Health <= 0);
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(t => t.NumberOfBadges))
+            {
+                Console.WriteLine(trainer.ToString());
+            }
+        }
+    }
+}
---
+++ 06_DefiningClasses/Exercises/09_PokemonTrainer/Trainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        private string name; 
+        private int numberOfBadges;
+        private List<Pokemon> pokemons;
+
+        private Trainer()
+        {    
+            this.NumberOfBadges = 0;
+
+            this.pokemons = new List<Pokemon>();
+        }
+
+        public Trainer(string name)
+            : this()
+        {
+            this.Name = name;
+        }
+
+        public List<Pokemon> Pokemons
+        {
+            get { return this.pokemons; }
+            set { this.pokemons = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int NumberOfBadges
+        {
+            get { return this.numberOfBadges; }
+            set { this.numberOfBadges = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.NumberOfBadges} {this.Pokemons.Count}";
+        }
+    }
+}
