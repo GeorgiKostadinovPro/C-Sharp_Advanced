9c6a3b700162edd607940628d0d1cd98a575314c
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-08 17:19:48 +0300

    Add files via upload

---
+++ 06_DefiningClasses/Exercises/04_OpinionPoll/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private readonly List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> Members => this.members;
+
+        public void AddMember(Person member)
+        {
+            if (!this.members.Contains(member))
+            {
+                this.members.Add(member);
+            }
+        }
+
+        public Person GetOldestMember()
+            => this.members.OrderByDescending(m => m.Age).FirstOrDefault();
+    }
+}
---
+++ 06_DefiningClasses/Exercises/04_OpinionPoll/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] memberInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = memberInfo[0];
+                int age = int.Parse(memberInfo[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person[] peopleWithAgeMoreThan30 = family.Members
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToArray();
+
+            foreach (var person in peopleWithAgeMoreThan30)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
