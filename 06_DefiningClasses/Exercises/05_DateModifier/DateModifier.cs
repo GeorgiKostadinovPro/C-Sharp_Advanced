f3efcd942b7aa26e485dcc03002e0933daabe9ff
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-08 17:20:06 +0300

    Add files via upload

---
+++ 06_DefiningClasses/Exercises/05_DateModifier/DateModifier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private string firstDate;
+        private string secondDate;
+
+        public DateModifier(string firstDate, string secondDate)
+        {
+            this.FirstDate = firstDate;
+            this.SecondDate = secondDate;
+        }
+
+        public string FirstDate
+        {
+            get { return this.firstDate; }
+            private set { this.firstDate = value; }
+        }
+
+        public string SecondDate
+        {
+            get { return this.secondDate; }
+            private set { this.secondDate = value; }
+        }
+
+        public int GetDaysDifference()
+        {
+            DateTime firstDate = DateTime.Parse(this.FirstDate, CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.Parse(this.SecondDate, CultureInfo.InvariantCulture);
+
+            TimeSpan daysDifference = firstDate.Subtract(secondDate);
+            return Math.Abs(daysDifference.Days);
+        }
+    }
+}
---
+++ 06_DefiningClasses/Exercises/05_DateModifier/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier(firstDate, secondDate);
+            int daysDifference = dateModifier.GetDaysDifference();
+
+            Console.WriteLine(daysDifference);
+        }
+    }
+}
