4448c6c6dbe08d2b1e92ff0c374f0ccc27e3cc3a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-09 14:52:31 +0300

    Add files via upload

---
+++ 07_Implementing_DoublyLinkedList/DoublyLinkedList/LinkedListItem.cs
@@ -0,0 +1,16 @@
+﻿namespace DoublyLinkedList
+{
+    public class LinkedListItem<T>
+    {
+        public LinkedListItem(T value)
+        {
+            this.Value = value;
+        }
+
+        public LinkedListItem<T> Prev { get; set; }
+
+        public LinkedListItem<T> Next { get; set; }
+
+        public T Value { get; private set; }
+    }
+}
