d1f674d636e84bf25919778d490cb61361d3f94b
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-11 14:36:13 +0300

    Add files via upload

---
+++ 09_Generics/Lab/03_GenericScale/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string firstStr = "George";
+            string secondStr = "Lyubo";
+
+            EqualityScale<string> equalityScale 
+                = new EqualityScale<string>(firstStr, secondStr);
+
+            bool areEqual = equalityScale.AreEqual();
+
+            Console.WriteLine(areEqual);
+        }
+    }
+}
