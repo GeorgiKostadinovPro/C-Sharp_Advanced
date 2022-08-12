f7b403cd7e8eb5cc578d29f0c60e1146a044fc5a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-12 15:45:17 +0300

    Add files via upload

---
+++ 09_Generics/Exercises/01_GenericBoxOfString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+}
---
+++ 09_Generics/Exercises/02_GenericBoxOfInteger/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private readonly T value;
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}";
+        }
+    }
+}
