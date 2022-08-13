30bf2cc8a9064c62ddc9970753eae9c3bd013e32
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-13 14:54:38 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Lab/04_BookComparer/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book /*: IComparable<Book>*/
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public IReadOnlyCollection<string> Authors { get; }
+
+        //public int CompareTo([AllowNull] Book other)
+        //{
+        //    int result = this.Year.CompareTo(other.Year);
+
+        //    if (result == 0)
+        //    {
+        //        result = this.Title.CompareTo(other.Title);
+        //    }
+
+        //    return result;
+        //}
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
