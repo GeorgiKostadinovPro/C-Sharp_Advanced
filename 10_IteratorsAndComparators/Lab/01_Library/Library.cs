a5cfc6516ce11c797c5ac90a12122c2d0521bf4a
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-13 14:53:50 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Lab/01_Library/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        private ICollection<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = books;
+        }
+    }
+}
