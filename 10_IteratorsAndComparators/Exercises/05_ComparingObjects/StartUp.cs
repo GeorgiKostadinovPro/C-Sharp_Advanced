71b0be58016e7a0ab6a8e7cf27e502583f4b3687
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-14 19:57:07 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Exercises/05_ComparingObjects/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                string[] personData = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+                string town = personData[2];
+
+                Person person = new Person(name, age, town);
+                people.Add(person);
+            }
+
+            int personIndex = int.Parse(Console.ReadLine()) - 1;
+            Person personToCompare = people[personIndex];
+            people.Remove(personToCompare);
+            int equalCounter = 1;
+            int nonEqualCounter = 0;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personToCompare) == 0)
+                {
+                    equalCounter++;
+                }
+                else
+                {
+                    nonEqualCounter++;
+                }
+            }
+
+            if (equalCounter > 1)
+            {
+                Console.WriteLine($"{equalCounter} {nonEqualCounter} {equalCounter + nonEqualCounter}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
