36e157cb01ee511dee17a7f5cf40979bee18a8d7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-14 19:57:19 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Exercises/06_EqualityLogic/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) != 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+
+            return this.Name == person.Name && this.Age == person.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+    }
+}
---
+++ 10_IteratorsAndComparators/Exercises/06_EqualityLogic/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> hashedPeople = new HashSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+
+                Person person = new Person(name, age);
+
+                sortedPeople.Add(person);
+                hashedPeople.Add(person);
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(hashedPeople.Count);
+        }
+    }
+}
