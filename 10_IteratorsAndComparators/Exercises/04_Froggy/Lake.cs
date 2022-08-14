55e38f2dcf1c167e07ad46a3e5ab1894494f3f11
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-14 19:56:47 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Exercises/04_Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Length; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            int lastIndex = this.stones.Length - 1;
+            int backwardStartIndex = lastIndex % 2 == 0 ? lastIndex - 1 : lastIndex;
+
+            for (int i = backwardStartIndex; i >= 0; i -= 2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 10_IteratorsAndComparators/Exercises/04_Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
