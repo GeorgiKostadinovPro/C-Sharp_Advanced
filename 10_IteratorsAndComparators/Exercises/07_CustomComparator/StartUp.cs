cae05ca7090025495cf54e336607025616ce9dd0
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-14 19:57:38 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Exercises/07_CustomComparator/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            Func<int, int, int> comparator = (first, second) =>
+            {
+                if (first % 2 == 0 && second % 2 != 0)
+                {
+                    return -1;
+                }
+                else if (first % 2 != 0 && second % 2 == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return first.CompareTo(second);
+                }
+            };
+
+            Array.Sort(numbers, new Comparison<int>(comparator));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
