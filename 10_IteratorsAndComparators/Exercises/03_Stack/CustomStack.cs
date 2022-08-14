37c192f2195dba790642c903f82eee7555a42894
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-14 19:56:26 +0300

    Add files via upload

---
+++ 10_IteratorsAndComparators/Exercises/03_Stack/CustomStack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] items;
+        private int count;
+
+        public CustomStack()
+        {
+            this.items = new T[InitialCapacity];
+            this.count = 0;
+        }
+
+        public int Count => count;
+
+        public void Push(T element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                T[] copy = new T[this.items.Length * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    copy[i] = this.items[i];
+                }
+
+                this.items = copy;
+            }
+
+            this.items[Count] = element;
+            this.count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T element = this.items[Count - 1];
+            this.items[Count - 1] = default(T);
+            this.count--;
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Custom stack is empty!");
+            }
+
+            T element = this.items[Count - 1];
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 10_IteratorsAndComparators/Exercises/03_Stack/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using CustomStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            string line = string.Empty;
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                if (line.Contains("Push"))
+                {
+                    string[] elements = line
+                        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Skip(1).ToArray();
+
+                    foreach (string element in elements)
+                    {
+                        stack.Push(int.Parse(element));
+                    }
+                }
+                else if (line == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            }
+
+            foreach (var number in stack)
+            {
+                Console.WriteLine(number);
+            }
+
+            foreach (var number in stack)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
