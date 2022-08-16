9d9113ddec313cd0eee9b10731bae1c5d5069ef7
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-16 16:07:14 +0300

    Add files via upload

---
+++ 11_AlgorithmsIntroduction/Exercises/02_RecursiveFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02_RecursiveFactorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Factorial(number);
+
+            Console.WriteLine(result);
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
