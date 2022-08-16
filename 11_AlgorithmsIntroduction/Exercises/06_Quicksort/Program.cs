b7e7e744fdfea8457034115258f6d3d10d6adc1d
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-08-16 16:08:05 +0300

    Add files via upload

---
+++ 11_AlgorithmsIntroduction/Exercises/06_Quicksort/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_Quicksort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Quick.Sort<int>(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+
+    public class Quick
+    {
+        public static void Sort<T>(T[] arr) where T : IComparable<T>
+        {
+
+            Shuffle(arr);
+            Sort(arr, 0, arr.Length - 1);
+        }
+
+        private static void Sort<T>(T[] arr, int lo, int hi) where T : IComparable<T>
+        {
+            if (lo >= hi)
+            {
+                return;
+            }
+
+            var part = Partition(arr, lo, hi);
+            Sort(arr, lo, part - 1);
+            Sort(arr, part + 1, hi);
+
+        }
+
+        private static int Partition<T>(T[] arr, int lo, int hi) where T : IComparable<T>
+        {
+            if (lo >= hi)
+            {
+                return lo;
+            }
+
+            var i = lo;
+            var j = hi + 1;
+
+            while (true)
+            {
+
+                while (IsLess(arr[++i], arr[lo]))
+                {
+                    if (i == hi)
+                    {
+                        break;
+                    }
+                }
+
+                while (IsLess(arr[lo], arr[--j]))
+                {
+                    if (j == lo)
+                    {
+                        break;
+                    }
+                }
+
+                if (i >= j)
+                {
+                    break;
+                }
+
+                Swap(arr, i, j);
+
+            }
+
+            Swap(arr, lo, j);
+            return j;
+        }
+
+        private static void Swap<T>(T[] arr, int i, int j) where T : IComparable<T>
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private static void Shuffle<T>(T[] arr) where T : IComparable<T>
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var r = i + random.Next(0, arr.Length - i);
+                Swap(arr, i, r);
+            }
+        }
+
+        private static bool IsLess<T>(T item1, T item2) where T : IComparable<T>
+        {
+            return item1.CompareTo(item2) < 0;
+        }
+    }
+}
---
+++ 11_AlgorithmsIntroduction/Exercises/07_BinarySearch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int elementToSearch = int.Parse(Console.ReadLine());
+
+            int index = BinarySearch.IndexOf(numbers, elementToSearch);
+
+            Console.WriteLine(index);
+        }
+    }
+    
+    public class BinarySearch
+    { 
+       public static int IndexOf(int[] nums, int elementToSearch)
+        {
+            Array.Sort(nums);
+
+            int start = 0;
+            int end = nums.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if (nums[mid] == elementToSearch)
+                {
+                    return mid;
+                }
+
+                if (nums[mid] < elementToSearch)
+                {
+                    start = mid + 1;
+                }
+                else if (nums[mid] > elementToSearch)
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
