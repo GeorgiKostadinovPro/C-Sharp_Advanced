0ca4bc3a9705c7bab1a71fe7fe972e83e7dfe2df
Georgi Kostadinov <kostadinovgeorgi16@gmail.com>
2022-11-10 17:59:42 +0200

    Update Program.cs

--- 03_SetsAndDictionariesAdvanced/Exercises/10_ForceBook/Program.cs
+++ 03_SetsAndDictionariesAdvanced/Exercises/10_ForceBook/Program.cs
@@ -15,7 +15,6 @@ namespace _10_ForceBook
 
             while ((line = Console.ReadLine()) != "Lumpawaroo")
             {
-
                 if (line.Contains("|"))
                 {
                     string[] cmdArgs = line
